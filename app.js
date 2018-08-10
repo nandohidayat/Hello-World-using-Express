a4f92ed20dc69aebe9704194efd18b9dd2756b7a
Nando <mnandohidayat@gmail.com>
2018-08-11 02:02:28 +0700

    use templates, set engine

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const app = express()
+const engines = require('consolidate')
+
+app.engine('html', engines.nunjucks)
+app.set('view engine', 'html')
+app.set('views', __dirname + '/views')
 
 app.get('/', (req, res) => {
-    res.send('Hello World')
+    res.render('hello', { 'name': 'Templates' })
 })
 
 app.use((req, res) => {
